2522e026ed317f4ae96767a6319941023fd619e8
nazarrudin-sp <133848919+nazarrudin-sp@users.noreply.github.com>
2023-10-06 13:07:37 +0700

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,31 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://npermana.github.io',
+	integrations: [
+		starlight({
+
+			title: 'Nazar Page',
+			social: {
+				github: 'https://github.com/npermana',
+				// linkedin: 'https://www.linkedin.com/in/evotianusb',
+			},
+			sidebar: [
+				{
+					label: 'Home',
+					link: '/'
+				},
+				{
+					label: 'Resume',
+					link: '/resume'
+				},
+				{
+					label: 'Projects',
+					autogenerate: { directory: 'projects' },
+				},
+			],
+		}),
+	],
+});
---
+++ src/content/docs/projects/Sveltekit/guide.md
@@ -0,0 +1,5 @@
+---
+title: Sveltekit Toy Project
+description: A reference page in my new Starlight docs site.
+---
+Add in some github pages action
